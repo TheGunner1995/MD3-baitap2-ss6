771ef4c86bbf9e2b5e11e76db605837af1bad68e
Tô Việt Hoàng <totranhieu@gmail.com>
2023-03-15 11:34:04 +0700

    aa

---
+++ src/Point3D.java
@@ -0,0 +1,40 @@
+public class Point3D extends Point2D {
+    private float z;
+
+    public Point3D() {
+    }
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float[] setXYZ() {
+        return new float[]{super.getX(), super.getY(), z};
+    }
+
+    public float[] getXYZ() {
+        return new float[]{super.getX(), super.getY(), z};
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + super.getX() +
+                "y=" + super.getY() +
+                "z=" + z +
+                '}';
+    }
+}
